449c6ea3bd0d15cbd6115e66f63b21ccd69f8d15
Alejandro Dinamarca <dinamarcalejandro@gmail.com>
2025-09-02 16:09:46 -0400

    🐛 fix(api): add missing swagger properties

--- src/cities/dto/create-city.dto.ts
+++ src/cities/dto/create-city.dto.ts
@@ -1,10 +1,14 @@
 import { IsString, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCityDto {
+
+  @ApiProperty({example: "Los Ángeles"})
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty()
   @IsString()
   code: string;
 }
