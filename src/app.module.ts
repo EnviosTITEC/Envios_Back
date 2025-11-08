4137022ed09de22b5e9f4d4df94ed792e5fe205e
Alejandro Dinamarca <dinamarcalejandro@gmail.com>
2025-11-08 14:27:38 -0300

    🚚 chore: remove auth logic as is out of concerns of this repo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
-import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AddressModule } from './addresses/addresses.module';
 import { CityModule as CountriesModule } from './cities/cities.module';
@@ -27,7 +26,6 @@ import { PostalModule } from './postal/postal.module'; // <-- asegúrate que est
     }),
 
     // módulos de la app
-    AuthModule,
     UsersModule,
     AddressModule,
     CountriesModule,
